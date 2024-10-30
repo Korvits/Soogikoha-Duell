506d2984ef86e38c1191e1b134d82512dbb5aafd
Marek Kalda <marek.kalda5@gmail.com>
2024-10-30 14:52:12 +0200

    Lisasin Pythoni jaoks folderid

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt
+# Teema: Söögikoha-Duell
+#
+#
+# Autorid: Marek Kalda, Elisee Kask
+#
+# 
+# mõningane eeskuju:
+#
+# Lisakommentaar (nt käivitusjuhend):
+#
+##################################################
