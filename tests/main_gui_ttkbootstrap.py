f2ede615edac4031f32671d62ed961bc46edaf4d
Marek Kalda <marek.kalda5@gmail.com>
2024-12-04 13:38:42 +0200

    Lisasin toidukohtadele rohkem infot ja muutsin kujundust

--- tests/main_gui_ttkbootstrap.py
+++ tests/main_gui_ttkbootstrap.py
@@ -9,6 +9,16 @@ def söögikohad_failist(failinimi): #alg koodist lihtsalt
             söögikohad.append(rida.strip())
     return söögikohad
 
+def söögid_failist(failinimi):
+    söögid = {}
+    with open(failinimi, 'r', encoding="UTF-8") as fail2:
+        for rida in fail2:
+            osad = rida.strip().split(";")
+            söögikoht = osad[0]
+            ühe_koha_söögid = osad[1:]
+            söögid[söögikoht] = ühe_koha_söögid
+    return söögid
+
 def alusta():
     global tervitus_label, nupp #see global phm selleks et ma saaks muuta siin funktsioonis mingit eelnevat funktsiooni v muutujat vms
     tervitus_label.grid_forget()  # Eemaldame tervituslabeli
@@ -16,12 +26,16 @@ def alusta():
     esita_küsimus()
 
 def esita_küsimus(): 
-    global söögikoht_indeks, kasutaja_indeks, jah_nupp, ei_nupp, img_label, img_list
+    global söögikoht_indeks, kasutaja_indeks, jah_nupp, ei_nupp, img_label, img_list 
     if söögikoht_indeks < len(söögikohad):
-        küsimus_label.config(text=f"Kasutaja {kasutaja_indeks + 1}, kas sulle meeldib söögikoht '{söögikohad[söögikoht_indeks]}'?")
-        küsimus_label.grid(row=1, column=1, columnspan=1, padx=15, pady=15)
-        jah_nupp.grid(row=2, column=2, columnspan=1, padx=15, pady=15)
-        ei_nupp.grid(row=2, column=0, columnspan=1, padx=15, pady=15)
+        küsimus_label.config(text=f"'{söögikohad[söögikoht_indeks]}'")
+        küsimus_label.grid(row=1, column=1, columnspan=1)
+        
+        söögid_label.config(text="\n".join(söögid[söögikohad[söögikoht_indeks]]))
+        söögid_label.grid(row=2,column=1, columnspan=1, sticky="n")
+        
+        jah_nupp.grid(row=3, column=2, columnspan=1)
+        ei_nupp.grid(row=3, column=0, columnspan=1)
         
         window.bind("<Right>", lambda x:[salvesta_vastus("jah"), pilt_edasi(3)]) # Saab noolteks jah või ei panna
         window.bind("<Left>", lambda x:[salvesta_vastus("ei"), pilt_edasi(3)])
@@ -50,12 +64,13 @@ def alusta2_start():  # Teisele kasutajale lisame eraldi nupu ja teksti
     jah_nupp.grid_forget() 
     ei_nupp.grid_forget()
     nupp.grid_forget()
+    söögid_label.grid_forget()
 
-    kasutaja2_label = tb.Label(window, text="Kasutaja 2, nüüd on sinu kord vastata!", bootstyle="white")
-    kasutaja2_label.grid(row=1, column=1, columnspan=1, padx=15, pady=15)
+    kasutaja2_label = tb.Label(content_frame, text="Kasutaja 2, nüüd on sinu kord vastata!", bootstyle="white")
+    kasutaja2_label.grid(row=1, column=1, columnspan=1)
     
-    nupp2 = tb.Button(window, text="Alustame", command =lambda: [kasutaja2_alustab(), pilt_edasi(2)], bootstyle="primary, outline")
-    nupp2.grid(row=2, column=1, columnspan=1, padx=15, pady=15)
+    nupp2 = tb.Button(content_frame, text="Alustame", command =lambda: [kasutaja2_alustab(), pilt_edasi(2)], bootstyle="primary, outline")
+    nupp2.grid(row=2, column=1, columnspan=1)
     
     window.unbind("<Right>")
     window.unbind("<Left>")
@@ -74,10 +89,11 @@ def ühised_söögikohad():
     else:
         tulemus_label.config(text="Pole ühtegi ühist söögikohta, mis mõlemale meeldiks :(")
     küsimus_label.grid_forget()
-    jah_nupp.grid_forget() #remob nupud ja küsimused ära, et lõpp oleks clean
+    jah_nupp.grid_forget()
     ei_nupp.grid_forget()
     img_label.grid_forget()
-    tulemus_label.grid(row=2, column=1, columnspan=1, padx=15, pady=15) 
+    söögid_label.grid_forget()
+    tulemus_label.grid(row=1, column=1, columnspan=1, padx=15, pady=15, sticky="nsew") 
 
 def pilt_edasi(pildi_nr):
     global img_label, jah_nupp, ei_nupp, img_list
@@ -87,26 +103,46 @@ def pilt_edasi(pildi_nr):
     
     jah_nupp.config(command=lambda: [salvesta_vastus("jah"), pilt_edasi(pildi_nr + 1)]) #Muudab nuppude funktsioone peale igat vajutust
     ei_nupp.config(command=lambda: [salvesta_vastus("ei"), pilt_edasi(pildi_nr + 1)])
+    
     window.bind("<Right>", lambda x:[salvesta_vastus("jah"), pilt_edasi(pildi_nr + 1)]) #Sama asi, aga noole nuppudele
     window.bind("<Left>", lambda x:[salvesta_vastus("ei"), pilt_edasi(pildi_nr + 1)])
     
 def main():
-    global söögikohad, kasutaja_vastused, kasutaja_indeks, söögikoht_indeks
+    global söögikohad, kasutaja_vastused, kasutaja_indeks, söögikoht_indeks, content_frame
     global window, küsimus_label, tulemus_label, tervitus_label, nupp, jah_nupp, ei_nupp
     global img_label, img_list
+    global söögid, söögid_label
     
     failinimi = "soogikohad.txt"
     söögikohad = söögikohad_failist(failinimi)
-    
-    
+    söögid_fail = "soogid.txt"
+    söögid = söögid_failist(söögid_fail)
 
     kasutaja_vastused = [[], []]
     kasutaja_indeks = 0
     söögikoht_indeks = 0
-
+    
     window = tb.Window(themename='superhero')
     window.title("Söögikoha duell")
     window.iconbitmap("images\logo.ico")
+    
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    
+    x = (screen_width / 2) - (1200 / 2)
+    y = (screen_height / 2) - (1000 / 2)
+    
+    window.geometry(f"1200x1000+{int(x)}+{int(y)}")
+    
+    content_frame = tb.Frame(window)
+    content_frame.pack(expand=True, fill="both")
+    
+    content_frame.grid_rowconfigure(0, weight=1)
+    content_frame.grid_rowconfigure(1, weight=1)
+    content_frame.grid_rowconfigure(2, weight=1)
+    content_frame.grid_rowconfigure(4, weight=1)
+    content_frame.grid_columnconfigure(0, weight=1)
+    content_frame.grid_columnconfigure(2, weight=1)
     
     my_img1 = Image.open("images/Logo.jpg").resize((640, 640))
     my_img1 = ImageTk.PhotoImage(my_img1)
@@ -142,22 +178,23 @@ def main():
     my_img11 = ImageTk.PhotoImage(my_img11)
     
     img_list = [my_img1, my_img2, my_img3, my_img4, my_img5, my_img6, my_img7, my_img8, my_img9, my_img10, my_img11]
-        
     
-    img_label = tb.Label(image=my_img1)
+    
+    img_label = tb.Label(content_frame, image=my_img1)
     img_label.grid(row=0, column=0, columnspan=3) #Kuvab pildi
 
+    tervitus_label = tb.Label(content_frame, text="Kas tunnete tihti, et ei suuda jõuda kokkuleppele, kuhu Tartus sööma minna? Las ma aitan Teid!", bootstyle="white")
+    tervitus_label.grid(row=1, column=1, columnspan=1, pady=15) # Kuvab algse küsimuse
     
-    tervitus_label = tb.Label(window, text="Kas tunnete tihti, et ei suuda jõuda kokkuleppele, kuhu Tartus sööma minna?\nLas ma aitan Teid!", bootstyle="white")
-    tervitus_label.grid(row=1, column=1, columnspan=1, padx=15, pady=15) # Kuvab algse küsimuse
+    nupp = tb.Button(content_frame, text="Kas alustame?", command=lambda: [alusta(), pilt_edasi(2)], bootstyle="primary, outline")
+    nupp.grid(row=2, column=1, columnspan=1, pady=15)
 
-    nupp = tb.Button(window, text="Kas alustame?", command=lambda: [alusta(), pilt_edasi(2)], bootstyle="primary, outline")
-    nupp.grid(row=2, column=1, columnspan=1, padx=15, pady=15)
-
-    küsimus_label = tb.Label(window, text="", bootstyle="white", wraplength=400)
-    tulemus_label = tb.Label(window, text="", bootstyle="white", wraplength=400)
-    jah_nupp = tb.Button(window, text="Jah", bootstyle="success", command=lambda:[salvesta_vastus("jah"), pilt_edasi(3)])
-    ei_nupp = tb.Button(window, text="Ei", bootstyle="danger", command=lambda:[salvesta_vastus("ei"), pilt_edasi(3)])
+    küsimus_label = tb.Label(content_frame, text="", bootstyle="white")
+    tulemus_label = tb.Label(content_frame, text="", bootstyle="white")
+    söögid_label = tb.Label(content_frame, text="", bootstyle="white")
+    
+    jah_nupp = tb.Button(content_frame, text="Jah", bootstyle="success", width=15, command=lambda:[salvesta_vastus("jah"), pilt_edasi(3)])
+    ei_nupp = tb.Button(content_frame, text="Ei", bootstyle="danger", width=15, command=lambda:[salvesta_vastus("ei"), pilt_edasi(3)])
     
 
     window.mainloop()
